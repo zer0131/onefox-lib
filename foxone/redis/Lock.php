38d702bc580febabffa6715b538a16c9d1ab5b4a
五环小胖子 <zer0131@vip.qq.com>
2017-10-16 15:48:58 +0800

    添加基于redis的锁实现

---
+++ foxone/redis/Lock.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @author ryan<zer0131@vip.qq.com>
+ * @desc 
+ * 基于redis锁实现
+ * redis扩展使用：phpredis<https://github.com/phpredis/phpredis>
+ */
+
+namespace foxone\redis;
+
+class Lock {
+
+    const LOCK_PREFIX = 'lock:';
+
+    private $_redis;
+
+    public function __construct() {
+        if (!extension_loaded('redis')) {
+            throw new \RuntimeException('The redis extension must be loaded.');
+        }
+        $this->_redis = new Redis();
+    }
+
+    public function acquire($lockName, $acquireTime = 10, $lockTimeout = 10) {
+        $value = uniqid();
+        $key = self::LOCK_PREFIX . $lockName;
+        $lockTimeout = intval($lockTimeout);
+        $end = time() + $acquireTime;
+        while (time() < $end) {
+            if ($this->_redis->setnx($key, $value)) {
+                $this->_redis->expire($key, $lockTimeout);
+                return $value;
+            } elseif (!$this->_redis->ttl($key)) {
+                $this->_redis->expire($key, $lockTimeout);
+            }
+            usleep(1000);
+        }
+        return false;
+    }
+
+    public function release($lockName, $value) {
+        $key = self::LOCK_PREFIX . $lockName;
+        if ($this->_redis->get($key) == $value) {
+            $this->_redis->del($key);
+            return true;
+        }
+        return false;
+    }
+
+    public function __destruct() {
+        $this->_redis->close();
+        $this->_redis && $this->_redis = NULL;
+    }
+}
